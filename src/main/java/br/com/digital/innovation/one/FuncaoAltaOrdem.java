5ba684fc05678181859a5e25f5869994cbe211a2
aguinaldo1 <aguinaldoamerico2@gmail.com>
2021-08-02 15:22:34 -0300

    acres. novas funcionalid

--- src/main/java/br/com/digital/innovation/one/FuncaoAltaOrdem.java
+++ src/main/java/br/com/digital/innovation/one/FuncaoAltaOrdem.java
@@ -1,12 +1,25 @@
 package br.com.digital.innovation.one;
 
 /*O paradigma funcional fala o que ele vai ter que receber, qual sera
-    o comportamento dele  */
+    o comportamento dele
+
+    Função de alta ordem -> é uma função que retorna uma função
+    ou que recebe uma função como parâmetro.*/
+
+
 
 public class FuncaoAltaOrdem {
     public static void main (String [] args){
         Calculo soma = (a,b) -> a+b;
+        Calculo subtracao = (a,b) -> a-b;
+        Calculo divisao = (a,b) -> a/+b;
+        Calculo mult = (a,b) -> a*b;
+
+
         System.out.println(executarOperacao(soma,1,3));
+        System.out.println(executarOperacao(subtracao,4,3));
+        System.out.println(executarOperacao(divisao,4,2));
+        System.out.println(executarOperacao(mult,7,3));
     }
 
 
@@ -16,7 +29,7 @@ public class FuncaoAltaOrdem {
     }
 }
 
-
+@FunctionalInterface
 interface Calculo{
     public int calculor (int a, int b);
 }
