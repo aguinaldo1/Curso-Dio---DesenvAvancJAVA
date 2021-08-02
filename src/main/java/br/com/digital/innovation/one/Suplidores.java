74e242e0cbb2b2d2f2240f492782cc6da31042e1
aguinaldo1 <aguinaldoamerico2@gmail.com>
2021-08-02 16:39:15 -0300

    first commit

---
+++ src/main/java/br/com/digital/innovation/one/Suplidores.java
@@ -0,0 +1,27 @@
+package br.com.digital.innovation.one;
+
+
+import java.util.function.Supplier;
+
+public class Suplidores {
+    public static void main(String[] args) {
+        Supplier<Pessoa> suppliers = () -> new Pessoa();
+        System.out.println(suppliers.get());
+    }
+}
+
+class Pessoa{
+    private String nome;
+    private Integer idade;
+
+    public Pessoa(){
+        nome = "Aguinaldo";
+        idade = 41;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("nome : %s, idade %d",nome,idade);
+    }
+}
+
